51b8f04c95939efd7b45f40de901f7a622dd7362
Jordan Revata <jordan.revata@tecsup.edu.pe>
2018-04-04 01:03:14 -0500

    Final commit

--- app/src/main/java/com/captureimage/captureimage/MainActivity.java
+++ app/src/main/java/com/captureimage/captureimage/MainActivity.java
@@ -1,22 +1,47 @@
 package com.captureimage.captureimage;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.util.List;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import pl.aprilapps.easyphotopicker.DefaultCallback;
 import pl.aprilapps.easyphotopicker.EasyImage;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    public static final int REQUEST_CODE_CAMERA     = 0012;
+    public static final int REQUEST_CODE_GALLERY    = 0013;
+
+    private CircleImageView profile_image;
     private Button button;
+
+    private String[] items = {"Camera" , "Gallery"};
+
+    private static final int PERMISSIONS_REQUEST = 100;
+
+    private static String[] PERMISSIONS_LIST = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +49,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
-        imageView = findViewById(R.id.imageView);
+        profile_image = findViewById(R.id.profile_image);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               
+               openImage();
             }
         });
+
+        checkAllPermissions();
+    }
+
+    private void checkAllPermissions() {
+        boolean permissionRequired = false;
+        for (String permission : PERMISSIONS_LIST){
+            if(ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED)
+                permissionRequired = true;
+        }
+        if(permissionRequired){
+            ActivityCompat.requestPermissions(this, PERMISSIONS_LIST, PERMISSIONS_REQUEST);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case PERMISSIONS_REQUEST: {
+                for (int i=0; i<grantResults.length; i++){
+                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                        Toast.makeText(this, PERMISSIONS_LIST[i] + " permissions declined!", Toast.LENGTH_LONG).show();
+                        new Handler().postDelayed(new Runnable() {
+                            public void run() {
+                                finishAffinity();
+                            }
+                        }, Toast.LENGTH_LONG);
+                    }
+                }
+            }
+        }
+
+
+    }
+
+    private void openImage() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Opciones");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                if(items[i].equals("Camera")){
+                    EasyImage.openCamera(MainActivity.this,REQUEST_CODE_CAMERA);
+                }else if(items[i].equals("Gallery")){
+                    EasyImage.openGallery(MainActivity.this,REQUEST_CODE_GALLERY);
+                }
+            }
+        });
+
+        AlertDialog dialog= builder.create();
+        dialog.show();
+
     }
 
     @Override
@@ -44,12 +124,22 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onImagesPicked(List<File> imagesFiles, EasyImage.ImageSource source, int type) {
-                //Handle the images
-                onPhotosReturned(imagesFiles);
+            public void onImagesPicked(@NonNull List<File> imageFiles, EasyImage.ImageSource source, int type) {
+                onPhotosReturned(imageFiles,type);
             }
 
-            private void onPhotosReturned(List<File> imagesFiles) {
+
+
+            private void onPhotosReturned(List<File> imageFile,int type) {
+                switch (type){
+                    case REQUEST_CODE_CAMERA:
+                        Picasso.get().load(imageFile.get(0)).into(profile_image);
+                        break;
+                    case REQUEST_CODE_GALLERY:
+                        Picasso.get().load(imageFile.get(0)).into(profile_image);
+                        break;
+                }
+
             }
 
 
